9f204ed4c0a4bceb72dd44163908f3b74d7871be
emigu777 <emily.guillou1@gmail.com>
2024-11-21 10:30:36 +0100

    added configuration file and scoreboard site

---
+++ Task1/scoreboard.py
@@ -0,0 +1,54 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+from itertools import chain
+
+st.set_page_config(layout="wide")
+st.title("Stats")
+st.header("Scoreboard")
+col1, col2 = st.columns(2)
+
+number = "0"
+
+def flatten_chain(matrix):
+    return list(chain.from_iterable(matrix))
+
+
+
+scores = pd.DataFrame({'Emily': [np.random.randint(1,15), np.random.rand(1,15)], 'Lena': [np.random.randint(1,15), np.random.rand(1,15)], 'Luna':[np.random.randint(1,15), np.random.rand(1,15)]}, index=["quantity","quality"])
+quantity = pd.DataFrame(scores, index=["quantity"])
+quantity = quantity.sort_values(["quantity"], axis = 1)
+with col1:
+    st.subheader("Scoreboard by quantity of guesses")
+    st.dataframe(quantity.T)
+
+
+quality =  scores.iloc[1]
+avg_qual = {}
+for index, value in quality.items():
+    value = flatten_chain(value)
+    avg_qual[index] = sum(value) / len(value)
+average_quality = pd.DataFrame(avg_qual, index = ["quality"])
+print(average_quality.T)
+average_quality.sort_values(["quality"], ascending = False, axis=1)
+
+with col2:
+    st.subheader("Scoreboard by quality of guesses")
+    st.dataframe(average_quality.T)
+
+## for score: +1 per guess for quantity, save quality of each guess and give mean quality back at the end
+st.header("Further Statistics")
+st.text("Number of Games played: " + number)
+
+st.text("Average number of guesses: ")
+
+print(quantity)
+print(average_quality)
+
+st.subheader("Number of guesses per game")
+st.bar_chart(scores)
+
+st.subheader("Average quality of guesses per game")
+#st.bar_chart(average_quality, x = average_quality.columns, y = average_quality["quality"])
+
+
